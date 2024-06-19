ebb73dd137febec6cffa3a36ac6626927f47b5f0
Bouffdaddy1z <derrickedus66@gmail.com>
2024-06-19 12:45:53 +0000

    created Home page

--- rn-assignment4-11115682/App.js
+++ rn-assignment4-11115682/App.js
@@ -1,9 +1,20 @@
-// App.js
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import AppNavigator from './Navigation/AppNavigator';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import 'react-native-gesture-handler';
+import Home from './Home';
+import Login from './Login';
 
-const App = () => {
-  return <AppNavigator />;
-};
+const Stack = createStackNavigator();
 
-export default App;
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='login' screenOptions={{headerShown: false}}>
+        <Stack.Screen name= 'login' component={Login}/>
+        <Stack.Screen name= 'Home' component = {Home}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
